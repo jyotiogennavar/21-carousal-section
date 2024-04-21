c80dcd18dd1f899aa52647388d44fd46e64c6403
Jyoti <jyoti.ogennavar@xecurify.com>
2024-04-21 10:23:16 +0530

    Remove unused test files and dependencies, add CarousalSection component

---
+++ src/CarousalSection/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+
+
+const Carousal = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Carousal
