c47ea8cd573a198c8c99cefc0d7952ed247f8524
Jyoti <jyoti.ogennavar@xecurify.com>
2024-04-21 19:26:01 +0530

    Add CardData.js file with person data for CarousalSection component

---
+++ src/CarousalSection/CardData.js
@@ -0,0 +1,52 @@
+export const persons = [
+  {
+    img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title:
+      "chief financial officer and executive vice president for global finance, aon plc",
+    name: "Christa Davies",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "revenue and financial management, stripe",
+    name: "Vivek Sharma",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=1061&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "head of product marketing, stripe",
+    name: "Tanya Khakbaz",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1554727242-741c14fa561c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "head of global sales, stripe",
+    name: "Eileen O'Mara",
+  },
+  {
+    img: "https://plus.unsplash.com/premium_photo-1670588775983-666b23590ffc?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "founder, pulley",
+    name: "Yin Wu",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1614644147798-f8c0fc9da7f6?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "ceo and cofounder, crowdai",
+    name: "Devaki Raj",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "co-head of payments, wix",
+    name: "Amit Sagiv",
+  },
+
+  {
+    img: "https://images.unsplash.com/photo-1639747280804-dd2d6b3d88ac?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "co-head of payments, wix",
+    name: "Amit Sagiv",
+  },
+
+  {
+    img: "https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "co-head of payments, wix",
+    name: "Amit Sagiv",
+  },
+];
+
+
--- src/CarousalSection/Index.jsx
+++ src/CarousalSection/Index.jsx
@@ -1,12 +1,251 @@
-import React from 'react'
-import styled from 'styled-components'
-import {motion} from 'framer-motion'
+import React, { useEffect, useRef, useState } from "react";
+import styled, { css } from "styled-components";
 
+import { persons } from "./CardData";
 
 const Carousal = () => {
-  return (
-    <div></div>
-  )
-}
+  const [activeItem, setActiveItem] = useState(1);
 
-export default Carousal
+  return (
+    <>
+      <Header>
+        <HeroTitle>Meet Our Speakers</HeroTitle>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud
+        </p>
+      </Header>
+      <Wrapper>
+        <CarousalContainer>
+          <PersonList>
+            {persons.map((person, index) => {
+              return (
+                <CarousalItem
+                  onClick={() => setActiveItem(index)}
+                  aria-current={activeItem === index}
+                  key={index}
+                >
+                  <ImageContainer>
+                    <CarousalImage src={person.img} alt={person.name} />
+
+                    <TextWrapper isActive={activeItem === index}>
+                      <Title>{person.title}</Title>
+                      <Name>{person.name}</Name>
+                    </TextWrapper>
+                  </ImageContainer>
+                </CarousalItem>
+              );
+            })}
+          </PersonList>
+        </CarousalContainer>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+
+
+  font-family: "Poppins", sans-serif;
+`;
+
+const Header = styled.header`
+ font-family: "Poppins", sans-serif;
+  padding: 1rem 2rem;
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  width: 1200px;
+  max-width: 100%;
+
+  p {
+    max-width: 50ch;
+    font-size: 1rem;
+    line-height: 1.5rem;
+  }
+`;
+
+const HeroTitle = styled.h2`
+  font-size: 3rem;
+  font-weight: 700;
+  line-height: 2rem;
+  margin-bottom: 0.5rem;
+  color: #333;
+`;
+
+const CarousalContainer = styled.div`
+  width: 1200px;
+  max-width: 100%;
+`;
+
+const PersonList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+
+  @media (min-width: 768px) {
+    height: 500px;
+    flex-direction: row;
+    gap: 1.5%;
+  }
+`;
+
+const CarousalItem = styled.li`
+  cursor: pointer;
+  width: 8%;
+  &:first-child {
+    width: 1%;
+  }
+  &:last-child {
+    width: 1%;
+  }
+  
+  ${({ activeItem, index }) => activeItem === index && css`
+    width: 48%;
+    @media (min-width: 768px) {
+      transition: width var(--transition, 200ms ease-in);
+    }
+  `}
+
+  @media (min-width: 768px) {
+    position: relative;
+
+    &:before {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: -10px;
+      right: -10px;
+      top: 0;
+      background-color: white;
+      display: none;
+    }
+
+    &:hover:not(:first-child):not(:last-child),
+    &:not(:hover):not(:first-child):not(:last-child):hover ~ & {
+      width: 7%;
+    }
+    
+    &:hover:not(:first-child):not(:last-child),
+    &:not(:hover):not(:first-child):not(:last-child):hover {
+      width: 12%;
+    }
+
+    &:first-child {
+      pointer-events: none;
+      .img:first-child {
+        opacity: 0;
+      }
+    }
+    
+    &:last-child {
+      pointer-events: none;
+      .img:last-child {
+        opacity: 0;
+      }
+    }
+  }
+`;
+
+// const CarousalItem = styled.li`
+//   position: relative;
+//   list-style: none;
+//   width: 8%;
+//   transition: width 0.5s ease;
+
+//   &::before {
+//     position: absolute;
+//     top: 0;
+//     bottom: 0;
+//     left: -10px;
+//     right: -10px;
+//     content: "";
+//   }
+
+//   &:first-child,
+//   &:last-child {
+//     width: 1%;
+//     pointer-events: none;
+//     opacity: 0.8;
+//   }
+
+//   &:hover:not(:first-child):not(:last-child) {
+//     width: 12%;
+//     & ~ li:not(:first-child):not(:last-child) {
+//       width: 7%;
+//     }
+//   }
+
+//   ${(props) =>
+//     props["aria-current"] &&
+//     css`
+//       width: 40%;
+//     `}
+// `;
+
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 1rem;
+  position: relative;
+`;
+
+const CarousalImage = styled.img`
+  position: absolute;
+  width: 590px;
+  height: 500px;
+  object-fit: cover;
+  object-position: center;
+  filter: blur(0.8);
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const TextWrapper = styled.div`
+  position: absolute;
+  left: 0.5rem;
+  top: 0.5rem;
+  width: 590px;
+  padding: 2rem;
+  transition: transform, opacity 300ms;
+
+  @media (min-width: 768px) {
+    position: absolute;
+    padding: 0;
+    left: 1rem;
+    top: 2rem;
+    transform: ${({ isActive }) =>
+      isActive ? "translateX(0)" : "translateX(4px)"};
+    opacity: ${({ isActive }) => (isActive ? "1" : "0")};
+
+    
+  }
+`;
+
+const Title = styled.p`
+  font-size: 0.875rem;
+  text-transform: uppercase;
+
+  @media (min-width: 768px) {
+    font-size: 1.125rem;
+  }
+`;
+
+const Name = styled.p`
+  font-size: 1.25rem;
+  font-weight: bold;
+
+  @media (min-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+export default Carousal;
